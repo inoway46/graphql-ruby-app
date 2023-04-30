c70ca723829b9bcddc475132b184931f17cd058c
Yuya <inoueyuya416@gmail.com>
2023-04-30 19:44:57 +0900

    refactor mutation spec

--- spec/graphql/mutations/create_link_spec.rb
+++ spec/graphql/mutations/create_link_spec.rb
@@ -1,25 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Mutations::CreateLink do
+  def perform(user: nil, **args)
+    Mutations::CreateLink.new(object: nil, field: nil, context: {}).resolve(**args)
+  end
+
   describe 'create link' do
     it 'link to be created' do
-      query_string = <<-GRAPHQL
-        mutation {
-          createLink(input: {
-            url: "https://example.com/model_test",
-            description: "model test",
-          }
-          ) {
-            id
-            url
-            description
-          }
-        }
-      GRAPHQL
-      result = GraphqlRubyAppSchema.execute(query_string)
-      expect(result.dig('data', 'createLink')).not_to be_blank
-      link = Link.find_by(url: 'https://example.com/model_test')
-      expect(link.present?).to be true
+      link = perform(url: 'http://example.com', description: 'description')
+      expect(link.persisted?).to be true
     end
   end
 end
